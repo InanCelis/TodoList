f2c92eb2632221477f9e8603c82dd0b3c242c70f
inancelis <celisinan@gmail.com>
2021-08-12 19:06:26 +0800

    Updated Dev.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <h1>This is an about page</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, placeat unde. Magni sint aperiam officia sapiente delectus temporibus, nostrum, porro eos fugit ducimus aliquid obcaecati quidem, id adipisci ipsam possimus. dd</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, placeat unde. Magni sint aperiam officia sapiente delectus temporibus, nostrum, porro eos fugit ducimus aliquid obcaecati quidem, id adipisci ipsam possimus. ddddd</p>
   </div>
 </template>
 
